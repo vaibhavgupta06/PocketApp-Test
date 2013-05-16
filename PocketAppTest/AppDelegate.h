925788bd33e5359610b991d05282479d989b449f
vaibhav <vaibhavgupta06@gmail.com>
2013-05-16 19:23:45 +0530

    Initial Commit

---
+++ PocketAppTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PocketAppTest
+//
+//  Created by Vaibhav Gupta on 16/05/13.
+//  Copyright (c) 2013 Vaibhav Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PocketAppTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PocketAppTest
+//
+//  Created by Vaibhav Gupta on 16/05/13.
+//  Copyright (c) 2013 Vaibhav Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
